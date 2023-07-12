a3903d01cd04b9c291e2b32c70ba26acd4f5daf1
Kurgon <kurgon@proton.me>
2023-07-12 06:13:11 -0400

    boilerplate SCSS

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import "../app.scss";
+</script>
+
+<slot />
+
+<style lang="scss" scoped>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to my website</h1>
+
+<style scoped lang="scss">
+  h1 {
+    margin-top: auto;
+  }
+</style>
