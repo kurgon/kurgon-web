10fa00dff12136298507a8f39bdbea19527a3c09
Kurgon <kurgon@proton.me>
2023-07-12 08:17:15 -0400

    +Routes for main pages

---
+++ src/routes/Contact/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import construction from "../../assets/under-construction.png";
+</script>
+
+<h1>Contact Me</h1>
+<img
+  style="height: 128px;aspect-ratio: 1/1;"
+  src={construction}
+  alt="Under construction"
+/>
---
+++ src/routes/Projects/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import construction from "../../assets/under-construction.png";
+</script>
+
+<h1>Projects</h1>
+<img
+  style="height: 128px;aspect-ratio: 1/1;"
+  src={construction}
+  alt="Under construction"
+/>
