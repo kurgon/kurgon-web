4066e40545b97d387e8d96343219dcd2bcf229d6
Kurgon <kurgon@proton.me>
2023-07-13 09:25:30 -0400

    +Add progress bar +Change progress bar color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
   import "../app.scss";
   import Navbar from "../components/Navbar.svelte";
+  import "nprogress/nprogress.css";
+  import "../nprogress.scss";
+  import NProgress from "nprogress";
+  import { navigating } from "$app/stores";
+
+  NProgress.configure({
+    minimum: 0.16,
+  });
+
+  $: {
+    if ($navigating) {
+      NProgress.start();
+    } else NProgress.done();
+  }
 </script>
 
 <Navbar />
