97b668436fe6bd6ed49bfffb5efd456d6c607065
kurgon <kurgon@proton.me>
2023-10-04 13:31:30 +1100

    +describe my goals and aspirations

---
+++ src/routes/About/+page.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+  import construction from "../../assets/under-construction.png";
+</script>
+
+<main>
+  <header>About</header>
+  <div class="divider" />
+  <p>
+    I am a software developer. I write code out of <span style="color: coral"
+      >passion</span
+    >, <span style="color: hotpink">activism</span> and
+    <span style="color: aquamarine">enjoyment</span>.
+  </p>
+  <p>
+    Computer science, mathematics and software freedom are my most passionate
+    topics.
+  </p>
+  <p>
+    I aspire to write code that combats state-sponsored and corporate
+    surveillance. My attention lies mainly in writing hobby projects, but as I
+    grow as a programmer, I hope to build programs that serve greater purposes.
+  </p>
+  <p>
+    My largest long term goal is to write an ethical open-source ecosystem of
+    software, replacing the likes of Microsoft and Google.
+  </p>
+  <p>Languages I am proficient in:</p>
+  <ol>
+    <li>Typescript</li>
+    <li>Rust</li>
+    <li>Sass/SCSS</li>
+    <li>HTML/CSS</li>
+  </ol>
+  <p>Aspirations:</p>
+  <ol>
+    <li>Learn Haskell</li>
+    <li>Cover holes in programming knowledge (finish cs50)</li>
+    <li>
+      Get better at comprehending code written by others (and my past self)
+    </li>
+  </ol>
+</main>
+
+<style scoped lang="scss">
+  main {
+    width: 90%;
+    height: 75%;
+    font-size: 1.4rem;
+    header {
+      font-size: 7rem;
+      font-weight: 800;
+    }
+    div.divider {
+      width: 100%;
+      height: 2px;
+      border: solid 2px #eceff440;
+      border-inline: none;
+    }
+    ol > li {
+      font-size: 1.2rem;
+    }
+  }
+</style>
